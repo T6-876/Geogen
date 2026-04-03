19ed6569214269890d8b8c2a88b846fdb8fb5353
T6-876 <kaidenlakeithrose@gmail.com>
2026-04-03 16:03:16 -0500

    Create ModernDecorationBuilder.hpp

---
+++ include/include/ModernDecorationBuilder.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+class ModernDecorationBuilder {
+
+public:
+
+    void build(EditorLayer* editor, float levelLength);
+
+};
