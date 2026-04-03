660ce61c937acaa7c9687c520b7c9f4053babbe4
T6-876 <kaidenlakeithrose@gmail.com>
2026-04-03 15:55:24 -0500

    Create WaveGameplayGenerator.hpp

---
+++ include/src/include/src/include/src/include/src/include/src/include/src/include/src/include/WaveGameplayGenerator.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+class WaveGameplayGenerator {
+
+public:
+
+    void generate(EditorLayer* editor, float startX);
+
+};
