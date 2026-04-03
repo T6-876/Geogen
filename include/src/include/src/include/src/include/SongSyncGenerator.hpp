f6ba767c7221757f9613fa1844cc37def453944a
T6-876 <kaidenlakeithrose@gmail.com>
2026-04-03 15:51:15 -0500

    Create SongSyncGenerator.hpp

---
+++ include/src/include/src/include/src/include/SongSyncGenerator.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+class SongSyncGenerator {
+
+public:
+
+    void addBeatTriggers(EditorLayer* editor, float levelLength);
+
+};
